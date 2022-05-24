027296dc05a18e394745312fa053fc35cb73fa78
ErikPervious <erickmatheus3000@gmail.com>
2022-05-24 14:54:10 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from 'React';
+import { View, Text, Button } from 'react-native';
+
+export default function App() {
+  return (
+    <View>
+      <Text>HeyGroups</Text>
+    </View>
+  );
+}
